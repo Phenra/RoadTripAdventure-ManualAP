c3bc961bcd66649b82801b140cb35041fe915fb2
Phenra <178694810+Phenra@users.noreply.github.com>
2025-11-02 00:28:17 -0700

    Updated build setup script to properly name executable

--- buildSetup.py
+++ buildSetup.py
@@ -3,14 +3,17 @@ from cx_Freeze import setup
 # This seems to be broken?
 # build_exe_options = {
 #     "include_files": [
-#         "./addresses.json"
+#         "addresses.json"
 #     ]
 # }
 
+name = "RoadTripAdventure-InventoryEditor"
+version = "0.1.0"
+
 setup(
-    name="RoadTripAdventure-InventoryEditor",
-    version="0.1.0",
+    name=name,
+    version=version,
     description="Live inventory editor for Road Trip Adventure, with additional functionality for manual Archipelago randomizer.",
     # options={"build_exe": build_exe_options},
-    executables=[{"script": "main.py", "base": "console"}],
+    executables=[{"script": "main.py", "base": "console", "target_name": f"{name}-{version}"}],
 )
