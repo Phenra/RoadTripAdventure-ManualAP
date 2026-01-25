a1f18c6dc2f668ddfe579148a9b55ce64fd51200
Phenra <178694810+Phenra@users.noreply.github.com>
2026-01-24 21:56:12 -0800

    Created rule hook for checking if player has access to enough cities for Coine rewards

--- manual_roadtripadventure_phenra/hooks/Rules.py
+++ manual_roadtripadventure_phenra/hooks/Rules.py
@@ -1,10 +1,34 @@
 from typing import Optional
 from worlds.AutoWorld import World
-from ..Helpers import clamp, get_items_with_value
+from ..Helpers import clamp, get_items_with_value, get_option_value
 from BaseClasses import MultiWorld, CollectionState
 
 import re
 
+def cityAccessCount(multiworld: MultiWorld, state: CollectionState, player: int, count: int):
+    if get_option_value(multiworld, player, "area_unlock_mode") == 0: # Decorations mode
+        return [
+            state.can_reach_region("Peach Town", player),
+            state.can_reach_region("Fuji City", player),
+            state.can_reach_region("Sandpolis", player),
+            state.can_reach_region("Chestnut Canyon", player),
+            state.can_reach_region("Mushroom Road", player),
+            state.can_reach_region("White Mountain", player),
+            state.can_reach_region("Papaya Island", player),
+
+            # Do not count Cloud Hill - this is used only for testing access to Q Coins for Coine's rewards
+            # state.can_reach_region("Cloud Hill", player)
+        ].count(True) >= count
+
+    elif get_option_value(multiworld, player, "area_unlock_mode") == 1: # Stamp mode
+        return state.count("Stamp", player) >= 5 * (count - 1)
+    
+    else:
+        raise Exception("Area Unlock Mode is not Decorations or Stamps, please fix your YAML.")
+
+
+# Example functions from Manual below:
+# -------------------------------------------
 # Sometimes you have a requirement that is just too messy or repetitive to write out with boolean logic.
 # Define a function here, and you can use it in a requires string with {function_name()}.
 def overfishedAnywhere(world: World, state: CollectionState, player: int):
